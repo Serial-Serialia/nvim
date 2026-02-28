9faa0cb9411a958ea68f5ebc4f7727c159d2d9ec
Serial-Serialia <jgabrielsoares04@gmail.com>
2026-02-28 12:42:05 -0300

    initilal commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+	"rebelot/kanagawa.nvim",
+	priority = 1000, -- Make sure to load this before all the other start plugins.
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("kanagawa").setup({
+			styles = {
+				comments = { italic = false }, -- Disable italics in comments
+			},
+		})
+		vim.cmd.colorscheme("kanagawa-wave")
+	end,
+}
